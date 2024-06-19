2c938f2a65dd6b1b7247278737bc23437cc81a3f
2E4O8T <benoit.froidevan@axa.fr>
2024-06-19 17:58:59 +0200

    commit-test

--- App Console/Program.cs
+++ App Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("My name is Lafouine !");
 Console.WriteLine("Mine  is Philgood !");
