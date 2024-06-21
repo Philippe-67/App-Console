cebea162248a72d01426aff333d7f1eff1850b00
Philippe CHARLES <A114SR@login.axa>
2024-06-21 16:16:57 +0200

    Personnalisation dictionnaire

--- App Console/Program.cs
+++ App Console/Program.cs
@@ -19,106 +19,113 @@ var newPerson = new Person
 Console.WriteLine($"Salut l'ami {newPerson.Firstname}");
 Console.WriteLine();
 
-// Lecture du dictionnary.txt pour écrire dans une nouvelle classe Mot
+// déclartion des variables composant le chemin et le nom du fichier user.firstnamedictionnary.txt 
 string filePath = "C:\\repos\\";
-string fileName = "dictionary.txt";
+string fileName = ($"{newPerson.Firstname}dictionary.txt");
 
+//instansiation d'une liste de mots 
 var newListMot = new List<Mot>();
+var line = "";
 
-StreamReader streamReader = new StreamReader(Path.Combine(filePath, fileName));
 
-// Lire la première ligne
-var line = streamReader.ReadLine();
-
-// Continuer à lire tant que line n'est pas null
-while (line != null)
+if (File.Exists(Path.Combine(filePath, fileName)))
 {
-    // Ecrire une nouvelle classe Mot
-
-    var newMot = new Mot();
-    newMot.MotFr = line;
-    newListMot.Add(newMot);
-
-    // Lire la ligne suivante
-    line = streamReader.ReadLine();
-}
-
-//Fermer le fichier dictionary.txt
-streamReader.Close();
-
-foreach (var mot in newListMot)
-{
-    Console.WriteLine($"Dans classe depuis fichier : {mot.MotFr}");
-}
-
-
-int nombreDeMotsEnChiffres = 666;
-
-while (nombreDeMotsEnChiffres < 1 || nombreDeMotsEnChiffres > 10)
-{
-    Console.Write("Combien de mots veux-tu ajouter (1-10) ?  ");
-
-    var nombreDeMots = Console.ReadLine();
-
-    nombreDeMotsEnChiffres = int.Parse(nombreDeMots);
-}
-
-Console.WriteLine($"Tu as choisis d'ajouter {nombreDeMotsEnChiffres} mots.");
-
-var listeMots = new List<Mot>();
-
-for (var i = 0; i < nombreDeMotsEnChiffres; i++)
-{
-    Console.Write($"Ajoute le mot N° {i + 1} : ");
-
-    var motEnPlus = Console.ReadLine();
-
-    var nouveauMot = new Mot()
+    ///Ouverture en mode lecture du fichier
+    using (StreamReader streamReader = new StreamReader(Path.Combine(filePath, fileName)))
     {
-        MotFr = motEnPlus
-    };
+        // Lire la première ligne
+        line = streamReader.ReadLine();
 
-    listeMots.Add(nouveauMot);
-}
+        // Continuer à lire tant que line n'est pas null
+        while (line != null)
+        {
+            // Ecrire une nouvelle classe Mot
 
-StreamWriter streamWriter = new StreamWriter(Path.Combine(filePath, fileName), true);
+            var newMot = new Mot();
+            newMot.MotFr = line;
+            newListMot.Add(newMot);
 
-// Ecrire les nouveaux mots dans la console et dans le dictionary.txt
-foreach (var mot in listeMots)
-{
-    Console.WriteLine($"La classe écrit : {mot.MotFr}");
-
-    streamWriter.WriteLine(mot.MotFr);
-    //using (StreamWriter outputFile = new StreamWriter(Path.Combine(filePath, fileName), true))
-    //{
-    //    outputFile.WriteLine(mot.MotFr);
-    //}
-}
-
-streamWriter.Close();
-
-// Lire le fichier dictionnary.txt
-StreamReader inputFile = new StreamReader(Path.Combine(filePath, fileName));
-
-// Lire la première ligne
-var line2 = inputFile.ReadLine();
-
-// Continuer à lire tant que line n'est pas null
-while (line2 != null)
-{
-    // Ecrire la ligne dans la console
-    Console.WriteLine($"Le fichier écrit : {line2}");
-
-    // Lire la ligne suivante
-    line2 = inputFile.ReadLine();
-}
-
-//Fermer le fichier dictionary.txt
-inputFile.Close();
+            // Lire la ligne suivante
+            line = streamReader.ReadLine();
+        }
 
 
 
-Console.WriteLine();
+        foreach (var mot in newListMot)
+        {
+            Console.WriteLine($"Dans classe depuis fichier : {mot.MotFr}");
+        }
+    } }
+
+
+else
+{ Console.WriteLine("nous allons creer un dictionnaire à ton nom"); }
+    int nombreDeMotsEnChiffres = 666;
+
+        while (nombreDeMotsEnChiffres < 1 || nombreDeMotsEnChiffres > 10)
+        {
+            Console.Write("Combien de mots veux-tu ajouter (1-10) ?  ");
+
+            var nombreDeMots = Console.ReadLine();
+
+            nombreDeMotsEnChiffres = int.Parse(nombreDeMots);
+        }
+
+        Console.WriteLine($"Tu as choisis d'ajouter {nombreDeMotsEnChiffres} mots.");
+
+        var listeMots = new List<Mot>();
+
+        for (var i = 0; i < nombreDeMotsEnChiffres; i++)
+        {
+            Console.Write($"Ajoute le mot N° {i + 1} : ");
+
+            var motEnPlus = Console.ReadLine();
+
+            var nouveauMot = new Mot()
+            {
+                MotFr = motEnPlus
+            };
+
+            listeMots.Add(nouveauMot);
+        }
+
+    
+        ///ouverture en mode écriture du fichier userfirstnamedictionnary 
+        StreamWriter streamWriter = new StreamWriter(Path.Combine(filePath, fileName), true);
+
+        // affichage des nouveaux mots dans la console et consignation dans  le dictionnary.txt
+        foreach (var mot in listeMots)
+        {
+            Console.WriteLine($"La classe écrit : {mot.MotFr}");
+            streamWriter.WriteLine(mot.MotFr);
+        }
+        ///fermeture de userfirstnamedictionnary
+        streamWriter.Close();
+
+        // Lire le fichier dictionnary.txt
+        StreamReader inputFile = new StreamReader(Path.Combine(filePath, fileName));
+
+        // Lire la première ligne
+        var line2 = inputFile.ReadLine();
+
+        // Continuer à lire tant que line n'est pas null
+        while (line2 != null)
+        {
+            // Ecrire la ligne dans la console
+            Console.WriteLine($"Le fichier écrit : {line2}");
+
+            // Lire la ligne suivante
+            line2 = inputFile.ReadLine();
+        }
+
+        //Fermer le fichier dictionary.txt
+        inputFile.Close();
+
+
+
+        Console.WriteLine();
+   
+
 
 // TODO
 // Check if the word already exists
